b7a694f1b6c26ebb656e2b9d28c27a68ae694c36
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-01-05 14:34:06 +0000

    feat: add waitFor async util

---
+++ src/asyncUtils/waitFor.ts
@@ -0,0 +1,20 @@
+let int: ReturnType<Window['setInterval']>
+
+export const waitFor = async (callback: () => boolean, interval = 100) =>
+  await new Promise((resolve) => {
+    if (callback()) {
+      resolve(true)
+    }
+
+    clearInterval(int)
+
+    int = setInterval(() => {
+      if (!callback()) {
+        return
+      }
+
+      clearInterval(int)
+
+      resolve(true)
+    }, interval) as unknown as number
+  })
