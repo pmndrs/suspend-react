79dd929455775bc0a5452590338ae4091cf48f23
drcmda <drcmda@gmail.com>
2022-01-10 13:11:09 +0100

    Update waitFor.ts

--- src/asyncUtils/waitFor.ts
+++ src/asyncUtils/waitFor.ts
@@ -2,19 +2,11 @@ let int: ReturnType<Window['setInterval']>
 
 export const waitFor = async (callback: () => boolean, interval = 100) =>
   await new Promise((resolve) => {
-    if (callback()) {
-      resolve(true)
-    }
-
+    if (callback()) resolve(true)
     clearInterval(int)
-
     int = setInterval(() => {
-      if (!callback()) {
-        return
-      }
-
+      if (!callback()) return
       clearInterval(int)
-
       resolve(true)
     }, interval) as unknown as number
   })
